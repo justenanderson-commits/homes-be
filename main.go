ea809af0e8866374e3014743214c90fbcba2af16
Justen Anderson <justenanderson@gmail.com>
2023-12-03 08:40:32 -0600

    Initial setup of main.go

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
 
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	dbHost := "mysql"
 }
